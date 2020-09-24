86674d947a313b06d9f1d5ee5a47503d6ddae01b
jobayer-h <jobayer.dev@gmail.com>
2020-09-24 23:07:13 +0600

    final tweak

--- src/Component/Header/UserAvatar.js
+++ src/Component/Header/UserAvatar.js
@@ -14,7 +14,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function UserAvatar(props) {
     const {photoURL} = props.signInUser;
-    console.log(photoURL);
 
   const classes = useStyles();
 
